c520c2def5f91cefe23d71f63bc263469b319f74
raphaelcmacedo <raphaelcmacedo@hotmail.com>
2016-07-10 10:56:14 -0300

    Angular routes configuration

--- WebContent/js/modules/main.js
+++ WebContent/js/modules/main.js
@@ -1,1 +1,16 @@
-angular.module('main',['rest']);
+var main = angular.module('main', ['ngRoute','rest']);
+
+//configure our routes
+main.config(function($routeProvider) {
+    $routeProvider
+
+        // index
+        .when('/', {
+            templateUrl : 'pages/list.html'
+        })
+        
+        // detail
+        .when('/detail', {
+            templateUrl : 'pages/detail.html'
+        });
+});
